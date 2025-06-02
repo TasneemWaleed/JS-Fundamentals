257c09961c5e54184d87595271c680709ad617ad
Tasneem Waleed <tasneem1waleed@gmail.com>
2025-06-02 23:43:28 +0300

    Add Task 7: print C is fun x times

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num)) {
+  console.log('MIssing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
+
